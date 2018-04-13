65bcbf0505c56d38ed0a8cdc297d473735295350
justasitsounds <james.prendergast@gmail.com>
2018-04-13 10:29:03 +1000

    fix smoothing test

--- main_test.go
+++ main_test.go
@@ -10,5 +10,10 @@ func TestSmoothing(t *testing.T) {
 	for i := 0; i <= limit; i++ {
 		fmt.Printf("smoothing(%v, %v) = %v\n", i, limit, smoothing(i, limit))
 	}
-	t.FailNow()
+	if smoothing(0, limit) != 1.0 {
+		t.Fatalf("expected %v but got %v", 1.0, smoothing(0, limit))
+	}
+	if smoothing(limit, limit) != 0 {
+		t.Fatalf("expected %v but got %v", 0, smoothing(limit, limit))
+	}
 }
